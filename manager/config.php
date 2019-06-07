b0cf1fbc6ab6ddf326a62fd4c6607cfcfecc3418
senku-devPC <ishibashi@senku.jp>
2019-06-07 17:21:39 +0900

    物理ファイル構造変更

---
+++ manager/config.php
@@ -0,0 +1,9 @@
+<?php
+// date_default_timezone_set('Asia/Tokyo');
+
+/* データベース関連 */
+define('DB_NAME', 'short_url');
+define('DB_HOST', 'localhost');
+define('DB_USER', '');
+define('DB_PASSWD', '');
+define('TABLE_PREFIX', 'surl_');
---
+++ manager/include/class/DBAPI.class.inc.php
@@ -0,0 +1,6 @@
+<?php
+include_once('DB.class.extends.php');
+
+class DBAPI extends DB {
+
+}
